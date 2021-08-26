7592fceef6fc3965be6a497fb8bcc179961e24fa
faithful1 <faithfulanere@gmail.com>
2021-08-26 10:21:58 +0800

    more outputs for the compute module

--- compute/output.tf
+++ compute/output.tf
@@ -1,1 +1,6 @@
 # --- compute/output.tf ---
+
+output "instance" {
+    value = aws_instance.k3_node[*] // this will output everything and let us access it through the root output
+    sensitive = true
+}
